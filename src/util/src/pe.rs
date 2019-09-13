623c70ab37dc199a339f3492bb8b9cf1ec498259
Ben Collins <aggieben@gmail.com>
2019-09-13 18:02:25 -0500

    add parsing function for PE file header

--- src/util/src/pe.rs
+++ src/util/src/pe.rs
@@ -1,8 +1,16 @@
-use nom::{IResult, dbg_dmp};
-use nom::bytes::complete::{tag};
-use nom::number::complete::le_u32;
+use nom::{IResult};
+use nom::bytes::complete::*;
+use nom::number::complete::*;
 
-fn has_msdos_header(input:&[u8]) -> IResult<&[u8], u32> {
+#[derive(Debug)]
+struct FileHeader {
+    num_sections : u16,
+    timestamp : u32,
+    opt_header_sz : u16,
+    flags : u16
+}
+
+fn validate_msdos_header(input:&[u8]) -> IResult<&[u8], u32> {
     const DOS_BEGIN : [u8; 60] = 
         [0x4d, 0x5a, 0x90, 0x00, 0x03, 0x00, 0x00, 0x00,
          0x04, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00,
@@ -29,17 +37,43 @@ fn has_msdos_header(input:&[u8]) -> IResult<&[u8], u32> {
     Ok((input, lfa))
 }
 
+fn parse_pe_file_header(input:&[u8]) -> IResult<&[u8], FileHeader> {
+    const PE_MACHINE : [u8; 2] = [ 0x4c, 0x01 ];
+
+    println!("starting to parse FileHeader");
+    let (input, _) = tag(&PE_MACHINE)(input)?;
+    println!("parsed machine");
+    let (input, num_sections) = le_u16(input)?;
+    println!("parsed num_sections");
+
+    let (input, timestamp) = le_u32(input)?;
+    println!("parsed timestamp");
+    let (input, _) = take(8usize)(input)?;
+    println!("skipped 8");
+    let (input, opt_hdr_sz) = le_u16(input)?;
+    println!("parsed opt_hdr_sz");
+    let (input, flags) = le_u16(input)?;
+    println!("parsed flags");
+
+    Ok((input, FileHeader {
+        num_sections: num_sections,
+        timestamp: timestamp,
+        opt_header_sz: opt_hdr_sz,
+        flags: flags
+    }))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const JSON_NET_ASSEMBLY : &[u8] = include_bytes!("../data/Newtonsoft.Json.dll");
+
     #[test]
-    fn pe_has_msdos_header_ok() {
-        let assembly_bytes = include_bytes!("../data/Newtonsoft.Json.dll");
+    fn validate_msdos_header_ok() {
+        let assembly_bytes = &JSON_NET_ASSEMBLY[0..128];
 
-        println!("Opened Newtonsoft.Json.dll; {} bytes", assembly_bytes.len());
-
-        let result = has_msdos_header(&assembly_bytes[..]);
+        let result = validate_msdos_header(assembly_bytes);
         match &result {
             Ok((rem, lfa)) => 
                 println!("Result: Ok, remaining input: {} bytes; lfa: {}", rem.len(), lfa),
@@ -50,5 +84,28 @@ mod tests {
         }
 
         assert!(result.is_ok());
+
+        let (remaining_input,_) = result.unwrap();
+        assert_eq!(remaining_input.len(), 0);
+    }
+
+    #[test]
+    fn parse_pe_file_header_ok() {
+        let section_bytes = &JSON_NET_ASSEMBLY[0x84..0x84 + 20];
+
+        let result = parse_pe_file_header(section_bytes);
+        match &result {
+            Ok((rem, header)) => 
+                println!("Result: Ok, remaining input: {} bytes; FileHeader: {:?}", rem.len(), header),
+            Err(nom::Err::Error((_, kind))) => 
+                println!("Result: Err, error: {:?}", kind),
+            _ => 
+                println!("Other error.")
+        }
+
+        assert!(result.is_ok());
+
+        let (remaining_input,_) = result.unwrap();
+        assert_eq!(remaining_input.len(), 0);
     }
 }
