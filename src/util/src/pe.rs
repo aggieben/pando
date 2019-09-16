ae7048d0dba57863814ac9adfc2a9e13be4496df
Ben Collins <aggieben@gmail.com>
2019-09-16 09:08:21 -0500

    add definition for Optional Header, as well as an enum for Machine

--- src/util/src/pe.rs
+++ src/util/src/pe.rs
@@ -5,11 +5,41 @@ use nom::number::complete::*;
 
 #[derive(Debug)]
 #[allow(dead_code)]
-struct FileHeader {
+struct CoffFileHeader {
+    machine : u16,
     num_sections : u16,
     timestamp : u32,
-    opt_header_sz : u16,
+    opt_header_size : u16,
     flags : Option<HeaderFlags>
+}
+
+#[repr(u16)]
+enum Machine {
+    IMAGE_FILE_MACHINE_UNKNOWN = 0,
+    IMAGE_FILE_MACHINE_AM33 = 0x1d3,        // Matsushita AM33
+    IMAGE_FILE_MACHINE_AMD64 = 0x8664,      // x64
+    IMAGE_FILE_MACHINE_ARM = 0x1c0,         // ARM little endian
+    IMAGE_FILE_MACHINE_ARM64 = 0xaa64,      // ARM64 little endian
+    IMAGE_FILE_MACHINE_ARMNT = 0x1c4,       // ARM Thumb-2 little endian
+    IMAGE_FILE_MACHINE_EBC = 0xebc,         // EFI byte code
+    IMAGE_FILE_MACHINE_I386 = 0x14c,        // Intel 386 or later processors and compatible processors
+    IMAGE_FILE_MACHINE_IA64 = 0x200,        // Intel Itanium processor family
+    IMAGE_FILE_MACHINE_M32R = 0x9041,       // Mitsubishi M32R little endian
+    IMAGE_FILE_MACHINE_MIPS16 = 0x266,      // MIPS16
+    IMAGE_FILE_MACHINE_MIPSFPU = 0x366,     // MIPS with FPU
+    IMAGE_FILE_MACHINE_MIPSFPU16 = 0x466,   // MIPS16 with FPU
+    IMAGE_FILE_MACHINE_POWERPC = 0x1f0,     // Power PC little endian
+    IMAGE_FILE_MACHINE_POWERPCFP = 0x1f1,   // Power PC with floating point support
+    IMAGE_FILE_MACHINE_R4000 = 0x166,       // MIPS little endian
+    IMAGE_FILE_MACHINE_RISCV32 = 0x5032,    // RISC-V 32-bit address space
+    IMAGE_FILE_MACHINE_RISCV64 = 0x5064,    // RISC-V 64-bit address space
+    IMAGE_FILE_MACHINE_RISCV128 = 0x5128,   // RISC-V 128-bit address space
+    IMAGE_FILE_MACHINE_SH3 = 0x1a2,         // Hitachi SH3
+    IMAGE_FILE_MACHINE_SH3DSP = 0x1a3,      // Hitachi SH3 DSP
+    IMAGE_FILE_MACHINE_SH4 = 0x1a6,         // Hitachi SH4
+    IMAGE_FILE_MACHINE_SH5 = 0x1a8,         // Hitachi SH5
+    IMAGE_FILE_MACHINE_THUMB = 0x1c2,       // Thumb
+    IMAGE_FILE_MACHINE_WCEMIPSV2 = 0x169    // MIPS little-endian WCE v2
 }
 
 bitflags! {
@@ -31,6 +61,41 @@ bitflags! {
         const IMAGE_FILE_UP_SYSTEM_ONLY             = 0x4000;
         const IMAGE_FILE_BYTES_REVERSED_HI          = 0x8000;
     }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct OptionalHeader {
+    standard_fields : StandardFields,
+    windows_fields : WindowsFields,
+    dataa_directories : DataDirectories
+}
+
+#[derive(Debug)]
+struct StandardFields {
+    code_size : u32,
+    initialized_data_size : u32,
+    uninitialized_data_size : u32,
+    entry_point_rva : u32,
+    code_base : u32,
+    data_base : u32
+}
+
+#[derive(Debug)]
+struct WindowsFields {
+    image_base : u32,
+    section_alignment : u32,
+    file_alignment : u32,
+    image_size : u32,
+    header_size : u32
+}
+
+#[derive(Debug)]
+struct DataDirectories {
+    import_table : u64,
+    relocation_table : u64,
+    import_address_table : u64,
+    cli_header : u64
 }
 
 fn validate_msdos_header(input:&[u8]) -> IResult<&[u8], u32> {
@@ -60,11 +125,10 @@ fn validate_msdos_header(input:&[u8]) -> IResult<&[u8], u32> {
     Ok((input, lfa))
 }
 
-fn parse_pe_file_header(input:&[u8]) -> IResult<&[u8], FileHeader> {
-    const PE_MACHINE : [u8; 2] = [ 0x4c, 0x01 ];
+fn parse_pe_file_header(input:&[u8]) -> IResult<&[u8], CoffFileHeader> {
 
     println!("starting to parse FileHeader");
-    let (input, _) = tag(&PE_MACHINE)(input)?;
+    let (input, machine) = le_u16(input)?;
     println!("parsed machine");
     let (input, num_sections) = le_u16(input)?;
     println!("parsed num_sections");
@@ -78,10 +142,11 @@ fn parse_pe_file_header(input:&[u8]) -> IResult<&[u8], FileHeader> {
     let (input, flags) = le_u16(input)?;
     println!("parsed flags");
 
-    Ok((input, FileHeader {
+    Ok((input, CoffFileHeader {
+        machine: machine,
         num_sections: num_sections,
         timestamp: timestamp,
-        opt_header_sz: opt_hdr_sz,
+        opt_header_size: opt_hdr_sz,
         flags: HeaderFlags::from_bits(flags)
     }))
 }
@@ -133,7 +198,7 @@ mod tests {
 
         assert_eq!(header.num_sections, 3);
         assert_eq!(header.timestamp, 0xb669c63c);
-        assert_eq!(header.opt_header_sz, 0x00e0);
+        assert_eq!(header.opt_header_size, 0x00e0);
         assert_eq!(header.flags, Some(
             HeaderFlags::IMAGE_FILE_DLL
             |HeaderFlags::IMAGE_FILE_LARGE_ADDRESS_AWARE
